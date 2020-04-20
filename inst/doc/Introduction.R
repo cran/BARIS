6882a9c0b301805cbf8587c4fdf69e27d258926a
Mohamed El Fodil Ihaddaden <ihaddaden.fodeil@gmail.com>
2020-04-20 07:40:03 +0000

    version 1.1.0

--- inst/doc/Introduction.R
+++ inst/doc/Introduction.R
@@ -1,9 +1,34 @@
-## ---- include = FALSE----------------------------------------------------
+## ---- include = FALSE---------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ----setup---------------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
+#  
+#  install.packages("BARIS")
+#  
+
+## ----message=FALSE, warning=FALSE---------------------------------------------
 library(BARIS)
+
+BARIS_search(query = "Monuments Historiques Marseille")
+
+
+## -----------------------------------------------------------------------------
+
+BARIS_explain(datasetId = "5cebfa8306e3e77ffdb31ef5")
+
+
+
+## -----------------------------------------------------------------------------
+BARIS_resources(datasetId = "5cebfa8306e3e77ffdb31ef5")
+
+
+## -----------------------------------------------------------------------------
+
+data <- BARIS_extract(resourceId = "59ea7bba-f38a-4d75-b85f-2d1955050e53", format = "csv")
+
+head(data)
+
 
